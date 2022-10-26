5d38b548a447528c77ba9d0894cf7c0d3031f804
Adnan Samuel <adnan.samuel@schibsted.com>
2022-10-26 11:01:59 +0200

    added address related properties

--- ModelsProject/AccountModel.cs
+++ ModelsProject/AccountModel.cs
@@ -12,5 +12,9 @@ namespace ModelsProject
         public string  Name { get; set; }
         public string  Email { get; set; }
 
-    }
+		public string Street { get; set; }
+		public string PostalCode { get; set; }
+		public string City { get; set; }
+		public string Country { get; set; }
+	}
 }
