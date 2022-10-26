627b2575e73bed9187302af521c83318b19b0789
Adnan Samuel <adnan.samuel@schibsted.com>
2022-10-26 11:06:30 +0200

    convert the postalcode to integrer

--- ModelsProject/AccountModel.cs
+++ ModelsProject/AccountModel.cs
@@ -13,7 +13,7 @@ namespace ModelsProject
         public string  Email { get; set; }
 
 		public string Street { get; set; }
-		public string PostalCode { get; set; }
+		public int PostalCode { get; set; }
 		public string City { get; set; }
 		public string Country { get; set; }
 	}
